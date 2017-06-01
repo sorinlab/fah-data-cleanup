e85b11e2d87eab1485fc30bc7619412616aeb4ca
Xavier Martinez <xavier@sorin11.campus.ad.csulb.edu>
2017-06-01 16:02:29 -0700

    simple changes. use of __init__ to instantiate class attributes.

--- fah_data_compressor.py
+++ fah_data_compressor.py
@@ -7,10 +7,10 @@ For example,
 
 PROJ1797/                      PROJ1797/
 ├── RUN0                       ├── RUN0
-│   ├── CLONE0                 │   ├── CLONE0.ext
-│   ├── CLONE1                 │   ├── CLONE1.ext
-│   ├── CLONE10                │   ├── CLONE10.ext
-│   ├── CLONE178    becomes    │   ├── CLONE178.ext
+│   ├── CLONE0                 │   ├── CLONE0.ext
+│   ├── CLONE1                 │   ├── CLONE1.ext
+│   ├── CLONE10                │   ├── CLONE10.ext
+│   ├── CLONE178    becomes    │   ├── CLONE178.ext
 └── RUN1                       └── RUN1
 │   ├── CLONE12                │   ├── CLONE12.ext
 │   ├── CLONE13                │   ├── CLONE13.ext
@@ -25,24 +25,22 @@ import os
 import re
 from subprocess import call
 
-class FAHDataCompressor(object):
-    """Class for compressing F@H data."""
-    def get_cli_arguments(self):
-        """Get the command line arguments."""
-        argument_parser = argparse.ArgumentParser()
-        argument_parser.add_argument('-p', '--project',
-                                     metavar='PROJECT_PATH',
-                                     required=True,
-                                     dest='projectPath',
-                                     help='path to the root of the F@H project')
-        argument_parser.add_argument('-m', '--max-dirs',
-                                     type=int,
-                                     metavar='NUM',
-                                     dest='max_dirs_number',
-                                     help='max number of directories to process')
-        return argument_parser.parse_args()
 
-    def find_clone_directories(self, project_root):
+class FAHDataCompressor(object):
+
+    """Class for compressing F@H data."""
+
+    def __init__(self, projectPath, maxDirs):
+        """Initialization of class instance variables.
+
+        Attributes:
+            project_path    The project parent directory.
+            max_dirs        The max number of directories to process.
+        """
+        self.project_path = projectPath
+        self.max_dirs = maxDirs
+
+    def find_clone_directories(self):
         """Find CLONE* directories inside the project.
 
         This function returns a list of tuples,
@@ -50,7 +48,8 @@ class FAHDataCompressor(object):
         directory and the path to its parent directory.
         """
         clone_dirs = []
-        for dir_name, subdir_list, file_list in os.walk(project_root):
+        project_root = os.path.abspath(self.project_path)
+        for dir_name, subdir_list, _ in os.walk(project_root):
             if subdir_list and re.search(r'CLONE\d+$', dir_name) is None:
                 # a CLONE dir has 'CLONE{x}' at the end of its path
                 # and no subdir in it; if that's not the case,
@@ -63,15 +62,15 @@ class FAHDataCompressor(object):
 
         return clone_dirs
 
-    def compress(self, project_root, max_dirs_number):
+    def compress(self):
         """Start the compression job."""
-        absolute_project_root = os.path.abspath(project_root)
-        clone_dirs = self.find_clone_directories(absolute_project_root)
+        absolute_project_root = os.path.abspath(self.project_path)
+        clone_dirs = self.find_clone_directories()
 
         processed_dir_count = 0
         for clone_dir_name, parent_dir in clone_dirs:
             processed_dir_count += 1
-            if max_dirs_number is not None and processed_dir_count > max_dirs_number:
+            if self.max_dirs is not None and processed_dir_count > self.max_dirs:
                 break
 
             print 'Compressing %s/%s...' % (parent_dir, clone_dir_name),
@@ -83,11 +82,21 @@ class FAHDataCompressor(object):
 
     def run(self):
         """Run the compression."""
-        args = self.get_cli_arguments()
-        project_root = args.projectPath
-        max_dirs_number = args.max_dirs_number
-        self.compress(project_root, max_dirs_number)
+        self.compress()
 
 
 if __name__ == '__main__':
-    FAHDataCompressor().run()
+    ARGUMENT_PARSER = argparse.ArgumentParser()
+    ARGUMENT_PARSER.add_argument('-p', '--project',
+                                 metavar='PROJECT_PATH',
+                                 required=True,
+                                 dest='projectPath',
+                                 help='path to the root of the F@H project')
+    ARGUMENT_PARSER.add_argument('-m', '--max-dirs',
+                                 type=int,
+                                 metavar='NUM',
+                                 dest='max_dirs_number',
+                                 help='max number of directories to process')
+    ARGS = ARGUMENT_PARSER.parse_args()
+    COMPRESSOR = FAHDataCompressor(ARGS.projectPath, ARGS.max_dirs_number)
+    COMPRESSOR.run()
